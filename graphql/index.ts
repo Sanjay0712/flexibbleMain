cda2f998953993ce44bfe37eef12d2eaad291d0f
SanjayKumar Dhamodharan <130839721+Sanjay0712@users.noreply.github.com>
2023-07-27 20:09:51 +0530

    Update index.ts

--- graphql/index.ts
+++ graphql/index.ts
@@ -84,6 +84,36 @@ export const projectsQuery = `
   }
 `;
 
+export const allProjectsQuery = `
+  query getProjects($endcursor: String) {
+    projectSearch(first: 8, after: $endcursor) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          title
+          githubUrl
+          description
+          liveSiteUrl
+          id
+          image
+          category
+          createdBy {
+            id
+            email
+            name
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const getProjectByIdQuery = `
   query GetProjectById($id: ID!) {
     project(by: { id: $id }) {
